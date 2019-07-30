3d8a6e3718616222b73ff233ebbe1719d2447621
Blue Bean <sl8305@hotmail.com>
2019-07-29 18:55:17 -0700

    added most of the notes for index and javascript file

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+// .ready is used to make sure the javascript waits until the page is created. 
+// this is needed to make sure Jquery can refernce the correct elements after they are created. 
+// w/o this Jquery would reference elements before they are created and not "work"
 $(document).ready(function() {
 
+    // array of preset animals
     var animals = [
       "dog", "cat", "rabbit", "hamster", "skunk", "goldfish",
       "bird", "ferret", "turtle", "sugar glider", "chinchilla",
@@ -7,22 +11,42 @@ $(document).ready(function() {
       "capybara", "teacup pig", "serval", "salamander", "frog"
     ];
   
+    // function dynamically adds buttons and assigns classes and text to each button
     function populateButtons(arrayToUse, classToAdd, areaToAddTo) {
+      // Clears the div where the gifs will populate
+      // This ensures any unnecessary information is cleared at the start before any buttons are used to output gifs
       $(areaToAddTo).empty();
   
+      // loops through the array given to dynamically create the buttons and give them attributes
       for (var i = 0; i < arrayToUse.length; i++) {
+        // variable to reference the buttons created
         var a = $("<button>");
+        
+        // dynamically adding the class from the function's intake to the button created above
         a.addClass(classToAdd);
+
+
+        // gives the button a data type of the given array at the current index in the loop
         a.attr("data-type", arrayToUse[i]);
+        
+        // gives each button the name assosiated to the current index of the array
         a.text(arrayToUse[i]);
+        
+        // appends each button to the button div
         $(areaToAddTo).append(a);
       }
-  
     }
   
+    // The following is executed when a button with the class animal-button is clicked
+    // jquery and the .on method is used to reference the buttons on the page with the specified class
     $(document).on("click", ".animal-button", function() {
+      // jquery is used to reference the div with the id animals and clears it to ensure nothing was saved prior to the start. 
       $("#animals").empty();
+
+      // .removeClass is used to take off the class "active" from the 
       $(".animal-button").removeClass("active");
+      
+
       $(this).addClass("active");
   
       var type = $(this).attr("data-type");
